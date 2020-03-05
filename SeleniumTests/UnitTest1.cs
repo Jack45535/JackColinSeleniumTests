639f35403997447c9d70b3d5a3fadad681760dc7
Jack45535 <Jackelliotcarlson@gmail.com>
2020-03-05 12:43:52 -0800

    Data Driven changes

--- SeleniumTests/UnitTest1.cs
+++ SeleniumTests/UnitTest1.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
+using System.Diagnostics;
 
 namespace SeleniumTodos
 {
@@ -22,33 +23,63 @@ namespace SeleniumTodos
         {
         }
 
+        //[TestMethod]
+        //public void ReadingDataFromExcelTestMethod()
+        //{
+        //    try
+        //    {
+        //        ExcelReader.PopulateInCollection(@"C:\Users\jacke\source\repos\dotnet-sqldb-tutorial-master\SeleniumTests\ExcelSpreadsheets\DataDrivenTesting.xlsx");
+        //        Debug.WriteLine(ExcelReader.ReadData(1, "Name"));
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //    }
+        //}
+
+
         [TestMethod]
         [TestCategory("Chrome")]
         public void CreateNew()
         {
-            driver.Navigate().GoToUrl(appURL + "/");
-            driver.FindElement(By.LinkText("Create New")).Click();
-            driver.FindElement(By.Id("Description")).Click();
-            driver.FindElement(By.Id("Description")).Clear();
-            driver.FindElement(By.Id("Description")).SendKeys("Test");
-            driver.FindElement(By.XPath("//div[2]/div")).Click();
-            driver.FindElement(By.XPath("//input[@value='Create']")).Click();
-            driver.FindElement(By.XPath("//*[contains(.,'Test')]"));
+            ExcelReader.PopulateInCollection(@"C:\Users\jacke\source\repos\dotnet-sqldb-tutorial-master\SeleniumTests\ExcelSpreadsheets\DataDrivenTesting.xlsx");
+            var on = 0;
+            var i = 1;
+            do
+            {
+                try
+                {
+                    var test = ExcelReader.ReadData(i, "Todos");
+                    driver.Navigate().GoToUrl(appURL + "/");
+                    driver.FindElement(By.LinkText("Create New")).Click();
+                    driver.FindElement(By.Id("Description")).Click();
+                    driver.FindElement(By.Id("Description")).Clear();
+                    driver.FindElement(By.Id("Description")).SendKeys(test);
+                    driver.FindElement(By.XPath("//div[2]/div")).Click();
+                    driver.FindElement(By.XPath("//input[@value='Create']")).Click();
+                    driver.FindElement(By.XPath("//*[contains(.,$test)]"));
+                    i += 1;
+                }
+                catch (Exception e)
+                {
+                    on += 1;
+                }
+            } while (on == 0) ;
         }
 
-        [TestMethod]
-        [TestCategory("Chrome")]
-        public void CreateNewFail()
-        {
-            driver.Navigate().GoToUrl(appURL + "/");
-            driver.FindElement(By.LinkText("Create New")).Click();
-            driver.FindElement(By.Id("Description")).Click();
-            driver.FindElement(By.Id("Description")).Clear();
-            driver.FindElement(By.Id("Description")).SendKeys("Test");
-            driver.FindElement(By.XPath("//div[2]/div")).Click();
-            driver.FindElement(By.XPath("//input[@value='Create']")).Click();
-            driver.FindElement(By.XPath("//*[contains(.,'I will fail you!')]"));
-        }
+        //[TestMethod]
+        //[TestCategory("Chrome")]
+        //public void CreateNewFail()
+        //{
+        //    driver.Navigate().GoToUrl(appURL + "/");
+        //    driver.FindElement(By.LinkText("Create New")).Click();
+        //    driver.FindElement(By.Id("Description")).Click();
+        //    driver.FindElement(By.Id("Description")).Clear();
+        //    driver.FindElement(By.Id("Description")).SendKeys("Test");
+        //    driver.FindElement(By.XPath("//div[2]/div")).Click();
+        //    driver.FindElement(By.XPath("//input[@value='Create']")).Click();
+        //    driver.FindElement(By.XPath("//*[contains(.,'I will fail you!')]"));
+        //}
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
